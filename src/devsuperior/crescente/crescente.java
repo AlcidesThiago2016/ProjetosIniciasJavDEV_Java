f9a83883c69d886ff1be768bcbf03db2e17316b7
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2022-09-20 10:18:12 -0300

    Implement class crescente.java

---
+++ src/devsuperior/crescente/crescente.java
@@ -0,0 +1,32 @@
+package devsuperior.crescente;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class crescente {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.ENGLISH);
+        Scanner sc = new Scanner(System.in);
+
+        int x, y = 0;
+
+        System.out.println("Digite dois numeros: ");
+        x = sc.nextInt();
+        y = sc.nextInt();
+
+        while (x != y) {
+            if (x > y) {
+                System.out.println("DECRESCENTE");
+            } else {
+                System.out.println("CRESCENTE");
+            }
+
+            System.out.println("Digite outros dois numeros: ");
+            x = sc.nextInt();
+            y = sc.nextInt();
+        }
+
+        sc.close();
+    }
+}

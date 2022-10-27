7dc4c15f7c6da5603b0805dccc195cc39f805ee7
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2022-10-27 19:17:43 -0300

    Implement class forEach.java with vectors

---
+++ src/devsuperior/vetor/forEach.java
@@ -0,0 +1,28 @@
+package devsuperior.vetor;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class forEach {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.ENGLISH);
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+
+        System.out.print("Informe quantos nomes ira digitar: ");
+        n = sc.nextInt();
+
+        String[] names = new String[n];
+
+        for (int i = 0; i < names.length; i++) {
+            sc.nextLine();
+            System.out.printf("Digite o %d nome: ", i + 1);
+            names[i] = sc.nextLine();
+        }
+        for ( String search : names) {
+            System.out.println(search);
+        }
+    }
+}

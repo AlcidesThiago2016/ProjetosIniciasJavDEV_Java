016ed36156fef0ed38d643273e57dd12526d3b7b
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2022-11-02 22:01:17 -0300

    Implement class EmployeeList.java with vectors

--- src/Employee_List/entities/EmployeeList.java
+++ src/Employee_List/entities/EmployeeList.java
@@ -2,13 +2,9 @@ package Employee_List.entities;
 
 public class EmployeeList {
 
-    private Integer id;
-    private String name;
+    private final Integer id;
+    private final String name;
     private Double salary;
-
-    public EmployeeList() {
-
-    }
     public EmployeeList(Integer id, String name, Double salary) {
         this.id = id;
         this.name = name;
@@ -17,26 +13,6 @@ public class EmployeeList {
 
     public Integer getId() {
         return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
     }
 
     public void increaseSalary(double percent){

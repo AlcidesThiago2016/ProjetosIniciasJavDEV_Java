33726781bd4b703eb61109586700ff44c036bbfd
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2022-11-08 22:30:49 -0300

    Update matrizGeral.java

--- src/devsuperior/matriz_geral/matrizGeral.java
+++ src/devsuperior/matriz_geral/matrizGeral.java
@@ -10,7 +10,7 @@ public class matrizGeral {
         Locale.setDefault(Locale.ENGLISH);
         Scanner sc = new Scanner(System.in);
 
-        int n;
+        int n, linha, coluna;
         double sumPositive;
 
         System.out.print("Qual a ordem da matriz? ");
@@ -33,6 +33,49 @@ public class matrizGeral {
             }
         }
         System.out.printf("SOMA POSITIVOS: %.2f\n", sumPositive);
+        System.out.println();
+
+        System.out.print("Escolha uma linha: ");
+        linha = sc.nextInt();
+
+        System.out.print("LINHA ESCOLHIDA: ");
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%.1f ", mat[linha][i]);
+        }
+
+        System.out.printf("\n\nEscolha uma coluna: ");
+        coluna = sc.nextInt();
+
+        System.out.print("COLUNA ESCOLHIDA: ");
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%.1f ", mat[i][coluna]);
+        }
+
+        System.out.printf("\n\nDIAGONAL PRINCIPAL: ");
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%.1f ", mat[i][i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] < 0){
+                    mat[i][j] = Math.pow(mat[i][j], 2);
+                }
+            }
+        }
+
+        System.out.println("\n\nMATRIZ ALTERADA: ");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.printf("%.1f ", mat[i][j]);
+            }
+            System.out.println();
+        }
+
 
 
         sc.close();

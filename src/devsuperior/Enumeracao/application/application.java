1685b20b0f56391c4c1ba6aca2fbf654d7610d1f
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2022-11-10 16:09:25 -0300

    Implement enum with variables os1 e os2

--- src/devsuperior/Enumeracao/application/application.java
+++ src/devsuperior/Enumeracao/application/application.java
@@ -11,5 +11,18 @@ public class application {
         Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
 
         System.out.println(order);
+
+        OrderStatus os1 = OrderStatus.DELIVERED;
+
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+
+        OrderStatus os3 = OrderStatus.SHIPPED;
+
+        OrderStatus os4 = OrderStatus.PROCESSING;
+
+        System.out.println(os1);
+        System.out.println(os2);
+        System.out.println(os3);
+        System.out.println(os4);
     }
 }

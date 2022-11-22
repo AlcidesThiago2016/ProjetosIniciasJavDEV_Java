63f6a7a9d75f08aa53d10ab299f0dc0c268f16ae
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2022-11-22 13:24:04 -0300

    Implement exemple polimorfismo with class Program.java

--- src/devsuperior/heranca1/application/Program.java
+++ src/devsuperior/heranca1/application/Program.java
@@ -13,6 +13,7 @@ public class Program {
 
         // UPCASTING
 
+
         Account acc1 = bacc;
         Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
         Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);

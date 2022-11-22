76762dba8732688651f7b8105c9546859b80eea2
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2022-11-21 22:54:13 -0300

    Create SavingsAccount.java

--- src/devsuperior/heranca1/application/Program.java
+++ src/devsuperior/heranca1/application/Program.java
@@ -1,11 +1,38 @@
 package devsuperior.heranca1.application;
 
+import devsuperior.heranca1.entities.Account;
 import devsuperior.heranca1.entities.BusinessAccount;
+import devsuperior.heranca1.entities.SavingsAccount;
 
 public class Program {
     public static void main(String[] args) {
 
-        BusinessAccount businessAccount = new BusinessAccount();
+        Account acc = new Account(1001, "Alex", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+
+        // UPCASTING
+
+        Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+        Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+        // DOWNCASTING
+
+        BusinessAccount acc4 = (BusinessAccount) acc2;
+
+        // BusinessAccount acc5 =  (BusinessAccount) acc3;
+        if (acc3 instanceof  BusinessAccount){
+            BusinessAccount acc5 = (BusinessAccount) acc3;
+            acc5.loan(200.0);
+            System.out.println("Loan!");
+        }
+
+        if (acc3 instanceof SavingsAccount){
+            SavingsAccount acc5 = (SavingsAccount) acc3;
+            acc5.updateBalance();
+            System.out.println("Update!");
+        }
 
     }
 }

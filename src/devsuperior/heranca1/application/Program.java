c54916ac4e424c351616c0e16a1a2cf7c7a2ba63
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2022-11-21 22:19:29 -0300

    Create class Account.java and BusinessAccount.java with Program.java.

---
+++ src/devsuperior/heranca1/application/Program.java
@@ -0,0 +1,11 @@
+package devsuperior.heranca1.application;
+
+import devsuperior.heranca1.entities.BusinessAccount;
+
+public class Program {
+    public static void main(String[] args) {
+
+        BusinessAccount businessAccount = new BusinessAccount();
+
+    }
+}

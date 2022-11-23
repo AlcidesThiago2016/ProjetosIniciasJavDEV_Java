630c9e0c0509865dd8288fcf3e0420886694ed32
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2022-11-23 11:33:44 -0300

    Implement Program.java, Shape.java, Color.java, Rectangle.java or Circle.java with Metod Abstract

--- src/devsuperior/MetodosAbstratos/application/Program.java
+++ src/devsuperior/MetodosAbstratos/application/Program.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
 
+
         Locale.setDefault(Locale.ENGLISH);
         Scanner sc =  new Scanner(System.in);
 

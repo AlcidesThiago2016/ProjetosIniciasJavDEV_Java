aeeb4c80104d9b0aa804280ec0e3f5a5246f083a
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2022-11-24 09:54:34 -0300

    Implement exception try/catch.

---
+++ src/devsuperior/ExceçõesJava/ExceptionsTryCatch.java
@@ -0,0 +1,29 @@
+package devsuperior.ExceçõesJava;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExceptionsTryCatch {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.ENGLISH);
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid position!");
+        }
+        catch (InputMismatchException e){
+            System.out.println("Input error!");
+        }
+
+        System.out.println("Enf of program!");
+        sc.close();
+    }
+}

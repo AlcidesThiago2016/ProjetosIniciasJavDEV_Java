c637873f595260aedc41303cc3cc4f776fdb239a
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2022-11-29 14:28:49 -0300

    Update Program2.java to work with files.

--- src/devsuperior/TrabalhandoArquivos/Program2.java
+++ src/devsuperior/TrabalhandoArquivos/Program2.java
@@ -8,12 +8,8 @@ public class Program2 {
     public static void main(String[] args) {
 
         String path = "/home/thiago/Documentos/in";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
             String line = br.readLine();
 
@@ -23,18 +19,6 @@ public class Program2 {
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }

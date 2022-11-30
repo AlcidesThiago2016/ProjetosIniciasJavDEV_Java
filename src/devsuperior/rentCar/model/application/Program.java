84b35095dd7024d92cc21268d97cbb216dab934c
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2022-11-30 19:22:01 -0300

    Implement class Program.java for execution apllication

---
+++ src/devsuperior/rentCar/model/application/Program.java
@@ -0,0 +1,14 @@
+package devsuperior.rentCar.model.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.ENGLISH);
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}

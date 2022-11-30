924f889d7abb9d703a2bcc456a4c888203ac2841
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2022-11-30 19:15:29 -0300

    Create package Rental with class CarRental.java, Invoice.java and Vehicle.java

---
+++ src/devsuperior/rentCar/model/entities/CarRental.java
@@ -0,0 +1,11 @@
+package devsuperior.rentCar.model.entities;
+
+import java.time.LocalDateTime;
+
+public class CarRental {
+
+    private LocalDateTime star;
+    private LocalDateTime finish;
+
+
+}

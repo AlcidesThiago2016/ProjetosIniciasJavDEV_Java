7fa75a1589de206453e6be5b42d929327b4dda7f
alcid <alcides_thiago@hotmail.com>
2022-12-12 23:09:07 -0300

    Creation within the main class of an anonymous class to instantiate the comparator

--- src/devsuperior/Comparator/application/Program.java
+++ src/devsuperior/Comparator/application/Program.java
@@ -2,10 +2,7 @@ package devsuperior.Comparator.application;
 
 import devsuperior.Comparator.entities.Product;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 public class Program {
     public static void main(String[] args) {
@@ -18,9 +15,14 @@ public class Program {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
+        Comparator<Product> comparator = new Comparator<Product>() {
+            @Override
+            public int compare(Product p1, Product p2) {
+                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+            }
+        };
 
-
-        list.sort(new MyComparator());
+        list.sort(comparator);
 
         for (Product p : list) {
             System.out.println(p);

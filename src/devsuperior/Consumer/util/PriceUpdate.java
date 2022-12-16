90a67b3efcb67e47df920610a1c4925754964a07
alcid <alcides_thiago@hotmail.com>
2022-12-15 22:54:31 -0300

    Implement interface functional Consumer with class PriceUpdate.java

---
+++ src/devsuperior/Consumer/util/PriceUpdate.java
@@ -0,0 +1,12 @@
+package devsuperior.Consumer.util;
+
+import devsuperior.Consumer.entities.Product;
+
+import java.util.function.Consumer;
+
+public class PriceUpdate implements Consumer<Product> {
+    @Override
+    public void accept(Product p) {
+        p.setPrice(p.getPrice() * 1.1);
+    }
+}

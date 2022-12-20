07004746bc3ac2d0cf1d356e40d827c1d0e18e7f
alcid <alcides_thiago@hotmail.com>
2022-12-20 15:08:28 -0300

    Implement functions expression lambda with method static.

--- src/devsuperior/FunctionsLambda/application/Program.java
+++ src/devsuperior/FunctionsLambda/application/Program.java
@@ -19,8 +19,15 @@ public class Program {
         list.add(new Product("Tablet",350.50));
         list.add(new Product("Hd Case",80.50));
 
+        //Implement function class UpperCaseName
         List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
 
+        System.out.println("Impressão lista com nomes maiusculos utilizando a classe UpperCaseName");
         names.forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("Impressão lista com nomes maiusculos utilizando a função staticUpperCaseName");
+        List<String> names1 = list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList());
+        names1.forEach(System.out::println);
     }
 }

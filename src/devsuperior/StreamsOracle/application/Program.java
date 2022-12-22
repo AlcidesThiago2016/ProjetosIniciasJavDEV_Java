b260759c74db8e3819ce719dfa2c92d1fe5cea50
alcid <alcides_thiago@hotmail.com>
2022-12-21 21:50:25 -0300

    Implementing intermediate operations on the stream using the map method

--- src/devsuperior/StreamsOracle/application/Program.java
+++ src/devsuperior/StreamsOracle/application/Program.java
@@ -12,9 +12,19 @@ public class Program {
 
         Locale.setDefault(Locale.US);
 
-        //Implementando um Stream com operações intermediárias utilizando o metodo filter.
+        //Implementando um Stream com operações intermediárias utilizando o metodo filter, buscando todas as pessoas
+        //que nasceram no Brasil
         List<Pessoa> pessoas = new Pessoa().populacePerson();
         Stream<Pessoa> stream = pessoas.stream().filter(pessoa -> pessoa.getNationality().equals("Brasil"));
+        System.out.println("Nomes pessoas nascidas no Brasil: ");
         System.out.println(Arrays.toString(stream.toArray()));
+
+        //Implementando um Stream com operações intermediárias utilizando o metodo map, realizado um mapeamento com
+        //o intuito de obter apenas a idade das pessoas presentes no fluxo de dados.
+        Stream<Integer> stream1 = pessoas.stream()
+                .filter(pessoa -> pessoa.getNationality().equals("Brasil"))
+                .map(Pessoa::getAge);
+        System.out.println("Idades pessoas nascidas no Brasil: ");
+        System.out.println(Arrays.toString(stream1.toArray()));
     }
 }
--- src/devsuperior/StreamsOracle/entities/Pessoa.java
+++ src/devsuperior/StreamsOracle/entities/Pessoa.java
@@ -67,8 +67,6 @@ public class Pessoa {
 
     @Override
     public String toString() {
-        return "Pessoa{" +
-                "name='" + name + '\'' +
-                '}';
+        return name ;
     }
 }
